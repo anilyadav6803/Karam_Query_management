a3fee12713b2b5a5cbb628e4c5719544a13fd239
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-09-13 00:43:28 +0530

    sidenave style

--- frontapp/src/component/Sidenav.jsx
+++ frontapp/src/component/Sidenav.jsx
@@ -9,6 +9,8 @@ const Sidenav = () => {
             <Link className='nav-link my-3 ps-3' to={`/userdash/${id}`}><i className='fa fa-book'></i>Dashbord</Link>
             <Link className='nav-link my-3 ps-3' to={`/addquery/${id}`}><i className='fa fa-question'></i>Add Query</Link>
             <Link className='nav-link my-3 ps-3' to={`/viewpenquery/${id}`}><i className='fa fa-eye'></i>View Pending Query</Link>
+            <Link className='nav-link my-3 ps-3' to={`/viewproquery/${id}`}><i className='fa fa-eye'></i>View Problem Query</Link>
+            <Link className='nav-link my-3 ps-3' to={`/viewcomquery/${id}`}><i className='fa fa-eye'></i>View Completed Query</Link>
         </div>
     </div>
   )
