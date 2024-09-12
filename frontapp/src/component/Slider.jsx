4f764baa652df1f0dcc029a10e2d3f9919ed51ae
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-09-13 00:43:42 +0530

    update

--- frontapp/src/component/Slider.jsx
+++ frontapp/src/component/Slider.jsx
@@ -6,62 +6,82 @@ import s4 from "../component/img/home-slide-02_0.jpg";
 
 const Slider = () => {
   return (
-    <div className="row bg-dark text-light p-2">
-      <div className="col-sm-5"></div>
-      <div className="col-sm-6">
-        <div
-          id="carouselExampleControls"
-          class="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <img src={s1} class="d-block w-100" alt="..." />
-              <div className="s bg-danger w-50 position-absolute">Hello1</div>
+    <div className="row bg-dark text-light p-3">
+      {/* Text Section */}
+      <div className="col-lg-4 col-md-12 hero-text mb-4 mb-md-0">
+        <h1>
+          Saving <span className="highlight">millions of lives globally</span>
+        </h1>
+        <p>KARAM is a leading global PPE & Fall Protection solutions brand.</p>
+        <button className="btn btn-danger">Learn More</button>
+
+        <div className="action-buttons mt-4">
+          <button className="btn btn-danger mx-2">
+            <i className="fas fa-shopping-cart"></i> Karam Online product
+          </button>
+          <button className="btn btn-dark mx-2">
+            <i className="fas fa-newspaper"></i> News & Events
+          </button>
+        </div>
+      </div>
+
+      {/* Slider Section */}
+      <div className="col-lg-6 col-md-12 d-flex justify-content-center mb-4 mb-md-0">
+        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img src={s1} className="d-block w-100" alt="Slide 1" />
+              <div className="s bg-danger text-center rounded w-50 position-absolute">
+                Practical Work 1
+              </div>
             </div>
-            <div class="carousel-item">
-              <img src={s2} class="d-block w-100" alt="..." />
-              <div className="s bg-danger w-50 position-absolute">Hello2</div>
+            <div className="carousel-item">
+              <img src={s2} className="d-block w-100" alt="Slide 2" />
+              <div className="s bg-danger text-center rounded w-50 position-absolute">
+                Practical Work 2
+              </div>
             </div>
-            <div class="carousel-item">
-              <img src={s3} class="d-block w-100" alt="..." />
-              <div className="s bg-danger w-50 position-absolute">Hello3</div>
+            <div className="carousel-item">
+              <img src={s3} className="d-block w-100" alt="Slide 3" />
+              <div className="s bg-danger text-center rounded w-50 position-absolute">
+                Practical Work 3
+              </div>
             </div>
-            <div class="carousel-item">
-              <img src={s4} class="d-block w-100" alt="..." />
-              <div className="s bg-danger w-50 position-absolute">Hello3</div>
+            <div className="carousel-item">
+              <img src={s4} className="d-block w-100" alt="Slide 4" />
+              <div className="s bg-danger text-center rounded w-50 position-absolute">
+                Practical Work 4
+              </div>
             </div>
           </div>
 
           <button
-            class="carousel-control-prev"
+            className="carousel-control-prev"
             type="button"
             data-bs-target="#carouselExampleControls"
             data-bs-slide="prev"
           >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
           </button>
           <button
-            class="carousel-control-next"
+            className="carousel-control-next"
             type="button"
             data-bs-target="#carouselExampleControls"
             data-bs-slide="next"
           >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
           </button>
         </div>
       </div>
-      <div className="col-sm-1 my-auto">
-        <p className="fa-brands fs-4 fa-facebook border p-3 rounded-circle"></p>
-        <br />
-        <p className="fa-brands fs-4 fa-instagram border p-3 rounded-circle"></p>
-        <br />
-        <p className="fa-brands fs-4 fa-youtube border p-3 rounded-circle"></p>
-        <br />
-        <p className="fa-brands fs-4 fa-twitter border p-3 rounded-circle"></p>
-        <br />
+
+      {/* Social Media Icons Section */}
+      <div className="col-lg-2 col-md-12 d-flex justify-content-center align-items-center flex-column mb-4 mb-md-0">
+        <p className="fa-brands fs-4 fa-facebook border p-3 rounded-circle mb-2"></p>
+        <p className="fa-brands fs-4 fa-instagram border p-3 rounded-circle mb-2"></p>
+        <p className="fa-brands fs-4 fa-youtube border p-3 rounded-circle mb-2"></p>
+        <p className="fa-brands fs-4 fa-twitter border p-3 rounded-circle mb-2"></p>
       </div>
     </div>
   );
