f0146fb0bea021e7632c907010290887c1836dbf
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-09-13 00:45:06 +0530

    login style

--- frontapp/src/mainapp/Login.jsx
+++ frontapp/src/mainapp/Login.jsx
@@ -36,8 +36,17 @@ const Login = () => {
   return (
     <>
     <Navbar/>
-    <div className='row'>
-         <div className='col-md-6 mx-auto p-5 shadow-lg my-5 rounded-5'>
+    <div
+  className="row"
+  style={{
+    backgroundImage: "url('https://as1.ftcdn.net/v2/jpg/02/92/90/56/1000_F_292905667_yFUJNJPngYeRNlrRL4hApHWxuYyRY4kN.jpg')",
+    backgroundSize: "cover",        // Ensures the image covers the entire div
+    backgroundPosition: "center",   // Centers the image within the div
+    backgroundRepeat: "no-repeat",   // Prevents the image from repeating
+    height: "70vh"                  // Adjust height as needed
+  }}
+>
+         <div className='col-md-6 mx-auto bg-secondary  p-5 shadow-lg my-5 rounded-5'>
          <form  onSubmit={logcode}>
                 <h4><span className='text-danger text-decoration-underline '>Login</span> Form</h4>
                 
