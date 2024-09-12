bbcc390d5fe3406c693f17fbc8c2115bb95cc747
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-09-13 00:42:44 +0530

    check routing

--- frontapp/src/adminapp/Viewalluser.jsx
+++ frontapp/src/adminapp/Viewalluser.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import Adsidenav from '../component/Adsidenav'
 import { useEffect } from 'react'
+import Topnav from '../component/topnav' // Corrected the casing for the import
 
 const Viewalluser = () => {
 
@@ -23,14 +24,32 @@ useEffect(() => {
 
   return (
     <>
-    <div className='row vh-7'>
-        <div className='col-sm-12 py-5 bg-dark'></div>
+       <div className='row' style={{ height: '7vh' }}>
+    <div className='col-12 py-3 bg-dark'>
+        <Topnav /> {/* Ensure Topnav is displayed correctly */}
     </div>
+</div>
     <div className='row vh-93'>
-        <div className='col-2 py-5 h-100 scroll bg-danger'>
-          <Adsidenav/>
+    <div
+          className="col-2 h-100 scroll"
+          style={{
+            backgroundImage:
+              "url(https://img.freepik.com/free-vector/background-realistic-abstract-technology-particle_23-2148431735.jpg?w=996&t=st=1726163520~exp=1726164120~hmac=488b990be6aa3a4027c6347715e8081dede8ccf4944407e0a025f3e635819ce0)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Adsidenav />
         </div>
-        <div className='col-10 py-5 h-100 scroll bg-success'>
+        <div
+          className="col-10 py-5 scroll"
+          style={{
+            backgroundImage:
+              "url('https://img.freepik.com/free-vector/gradient-futuristic-background-with-connection-concept_23-2149104856.jpg?w=996&t=st=1726165644~exp=1726166244~hmac=97351864f55695d093c7bd104093bd6b12e662abf29183cabbc76b08835c1f23')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
             <div className='row h-100'>
                 <div className='col-md-6 table-responsibe mx-auto p-2 rounded-2 my-auto shadow-lg bg-light'>
                     <table className='table table-light'>
